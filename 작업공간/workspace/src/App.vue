a83abd23398a34149ff45e1cc4ddae89b88d9fb5
dahun3013 <dahun3013@gmail.com>
2021-12-17 17:35:25 +0900

    디렉토리 정리

---
+++ 작업공간/workspace/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app>
+      <div id="nav">
+        <router-link to="/">
+          Home
+        </router-link> |
+        <router-link to="/about">
+          About
+        </router-link>
+      </div>
+      <router-view/>
+    </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
