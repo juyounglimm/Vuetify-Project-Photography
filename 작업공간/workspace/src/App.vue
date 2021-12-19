e3b4386aa5aa5222d86c999a9180706a4721f918
dahun3013 <dahun3013@gmail.com>
2021-12-19 17:13:02 +0900

    설계 업로드

--- 작업공간/workspace/src/App.vue
+++ 작업공간/workspace/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-    <v-app>
-      <div id="nav">
-        <router-link to="/">
-          Home
-        </router-link> |
-        <router-link to="/about">
-          About
-        </router-link>
-      </div>
-      <router-view/>
-    </v-app>
+  <v-app>
+    <div id="nav">
+      <router-link to="/">
+        Home
+      </router-link> |
+      <router-link to="/about">
+        About
+      </router-link> |
+      <router-link to="/appBar">
+        AppBar
+      </router-link>
+    </div>
+    <router-view />
+  </v-app>
 </template>
 
 <script>
---
+++ 작업공간/workspace/src/views/AppBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-card color="basil">
+    <v-img
+      src="../assets/banner.jpg"
+      height="200px"
+    >
+      <v-card-title class="text-center justify-center py-6">
+        <h1 class="font-weight-bold text-h2 basil--text">
+          카페 소개 메인화면!!!
+        </h1>
+      </v-card-title>
+
+      <v-tabs
+        v-model="tab"
+        background-color="transparent"
+        color="basil"
+        grow
+      >
+        <v-tab
+          v-for="item in items"
+          :key="item"
+        >
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item"
+        >
+          <v-card
+            color="basil"
+            flat
+          >
+            <v-card-text>{{ text }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    tab: null,
+    items: [
+      '카페신청', '지역별 소개','카페소개', '인테리어', '커뮤니티','고객센터',
+    ],
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+
+  })
+}
+</script>
+
+<style>
+.basil {
+  background-color: #FFFBE6 !important;
+}
+.basil--text {
+  color: #356859 !important;
+}
+</style>
